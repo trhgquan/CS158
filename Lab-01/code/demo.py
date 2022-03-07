46a3ef3d61d505f19af683afc6eff04bff65b50e
hwangswan <mrthedzam@gmail.com>
2022-03-07 20:59:57 +0700

    Modified

--- Lab-01/code/demo.py
+++ Lab-01/code/demo.py
@@ -6,10 +6,11 @@ model = kenlm.Model('model.arpa')
 # sentence = "\"I can't understand why my grandmother never gambles.\""
 sentence = "my grandmother never gambles ."
 
-# true_score = math.pow(10.0, model.score(sentence))
+score = model.score(sentence)
 
-# print("Model score:", true_score)
-print("Model score:", model.score(sentence))
+print("Sentence score:", score)
+
+print("Sentence true score:", 10**score)
 
 perplexity = model.perplexity(sentence)
 
